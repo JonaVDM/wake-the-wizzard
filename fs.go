d39034fab673779be84114f78c001793a0239123
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-04-15 11:26:41 +0200

    Added static pages

---
+++ fs.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"path"
+)
+
+type CustomFS struct {
+	content embed.FS
+}
+
+func (c CustomFS) Open(name string) (fs.File, error) {
+	return c.content.Open(path.Join("frontend", name))
+}
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"embed"
 	"net/http"
 
 	"github.com/JonaVDM/wake-the-wizzard/server"
 	"github.com/gorilla/mux"
 )
 
+//go:embed frontend/*
+var static embed.FS
+
 func main() {
 	srv := server.Server{
-		Router: mux.NewRouter(),
+		Router:      mux.NewRouter(),
+		StaticFiles: CustomFS{static},
 	}
 
 	srv.Init()
