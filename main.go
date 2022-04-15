32372397da0354f8b5cb9b77be109c65fa860c41
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-04-15 10:54:29 +0200

    Added Wake on Lan Package

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/JonaVDM/wake-the-wizzard/wol"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) < 2 {
+		log.Fatal("No mac address provided")
+	}
+
+	mac := os.Args[1]
+	wol.SendWol(mac)
 }
---
+++ wol/wol.go
@@ -0,0 +1,49 @@
+package wol
+
+import (
+	"encoding/hex"
+	"net"
+)
+
+func SendWol(mac string) error {
+	msg, err := buildMessage(mac)
+	if err != nil {
+		return err
+	}
+
+	pc, err := net.ListenPacket("udp4", ":9")
+	if err != nil {
+		return err
+	}
+	defer pc.Close()
+
+	addr, err := net.ResolveUDPAddr("udp4", "255.255.255.255:9")
+	if err != nil {
+		return nil
+	}
+
+	_, err = pc.WriteTo(msg, addr)
+	return err
+}
+
+func buildMessage(mac string) ([]byte, error) {
+	msg := make([]byte, 0)
+
+	// Fill the first 6 bytes with FF
+	for i := 0; i < 6; i++ {
+		msg = append(msg, byte(255))
+	}
+
+	// Convert the mac address to bytes
+	macBytes, err := hex.DecodeString(mac)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	// Fill in the rest with the bytes of the mac
+	for i := 0; i < 16; i++ {
+		msg = append(msg, macBytes...)
+	}
+
+	return msg, nil
+}
